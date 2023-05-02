5f5f87428e29df0b0fe10a565807d679e5f74cc7
abezzi <abezzi@outlook.cl>
2023-05-02 18:07:39 -0400

    feat: async chat with multiple clients working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    // Allows multiple producers and consumers to all send and receive on a single channel
+    let (tx, _rx) = broadcast::channel(10);
 
     // loop to handle 'infinite' amount of clients
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         // moves each client to his independant task (async block)
         tokio::spawn(async move {
@@ -21,16 +26,29 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                // store the amount of bytes per line
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                // if the bytes read is equal to 0 means that is at the EOF
-                if bytes_read == 0 {
-                    break;
+                // multiple concurrent async and work with the first one that gets a result
+                tokio::select! {
+                    // store the amount of bytes per line
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        // because the read_line append each line it reads
+                        line.clear();
+                    }
+
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        // avoid seeing the message two times when you are the sender
+                        if addr != other_addr {
+                            // the socket writes the line back to the client
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-                // the socket writes the line back to the client
-                writer.write_all(line.as_bytes()).await.unwrap();
-                // because the read_line append each line it reads
-                line.clear();
             }
         });
     }
